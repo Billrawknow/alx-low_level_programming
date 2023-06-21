aa936f02e876469c67afb50efaf3271729b23a62
Billrawknow <bildadronoh@gmail.com>
2023-06-21 04:31:41 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void print_fibonacci_sequence(int n) {
+    int first = 1;
+    int second = 2;
+    int next;
+
+    printf("%d, %d", first, second);
+
+    for (int i = 3; i <= n; i++) {
+        next = first + second;
+        printf(", %d", next);
+        first = second;
+        second = next;
+    }
+
+    printf("\n");
+}
+
+int main(void) {
+    print_fibonacci_sequence(50);
+    return 0;
+}
