035a5daa1d9559e52bab8ebe9b16e331c5e6dcb8
Billrawknow <bildadronoh@gmail.com>
2023-06-21 09:34:05 -0700

    103-fibonaccci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -4,21 +4,21 @@ int main() {
     int first = 1;
     int second = 2;
     int next = 0;
-    int sum = 2; // Start with the sum as 2 (since the second term is even)
+    int sum = 2; /* Start with the sum as 2 (since the second term is even) */
 
-    printf("%d, %d, ", first, second); // Print the first two terms of the Fibonacci sequence
+    printf("%d, %d, ", first, second); /* Print the first two terms of the Fibonacci sequence */
 
     while (next <= 4000000) {
         next = first + second;
         if (next % 2 == 0) {
             sum += next;
         }
-        printf("%d, ", next); // Print the next term
+        printf("%d, ", next); /* Print the next term */
         first = second;
         second = next;
     }
 
-    printf("\nSum of even-valued terms: %d\n", sum); // Print the sum of even-valued terms
+    printf("\nSum of even-valued terms: %d\n", sum); /* Print the sum of even-valued terms */
 
     return 0;
 }
