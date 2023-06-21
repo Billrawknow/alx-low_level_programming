25c6052ef4af3abbd2e652048a1ffbcc7e0499cc
Billrawknow <bildadronoh@gmail.com>
2023-06-21 09:25:50 -0700

    103-fibonaccci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,27 +1,24 @@
 #include <stdio.h>
 
-int main(void)
-{
-    unsigned long first = 1;
-    unsigned long second = 2;
-    unsigned long next;
-    unsigned long sum = 2; // Start with the sum as 2 (since the second term is even)
+int main() {
+    int first = 1;
+    int second = 2;
+    int next = 0;
+    int sum = 2; // Start with the sum as 2 (since the second term is even)
 
-    printf("1, 2, "); // Print the first two terms of the Fibonacci sequence
+    printf("%d, %d, ", first, second); // Print the first two terms of the Fibonacci sequence
 
-    while (next <= 4000000)
-    {
+    while (next <= 4000000) {
         next = first + second;
-        if (next % 2 == 0)
-        {
+        if (next % 2 == 0) {
             sum += next;
         }
-        printf("%lu, ", next); // Print the next term
+        printf("%d, ", next); // Print the next term
         first = second;
         second = next;
     }
 
-    printf("\nSum of even-valued terms: %lu\n", sum); // Print the sum of even-valued terms
+    printf("\nSum of even-valued terms: %d\n", sum); // Print the sum of even-valued terms
 
     return 0;
 }
