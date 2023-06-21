83b75e9dcb139a3be9da61e9ed52ee8db0cbbcf2
Billrawknow <bildadronoh@gmail.com>
2023-06-21 04:07:29 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * calculate_sum - Calculates the sum of multiples of 3 or 5
+ *
+ * Return: The sum of multiples of 3 or 5
+ */
 int calculate_sum(void)
 {
 	int total_sum = 0;
@@ -14,9 +19,15 @@ int calculate_sum(void)
 	return (total_sum);
 }
 
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
 	int result = calculate_sum();
+
 	printf("%d\n", result);
 
 	return (0);
