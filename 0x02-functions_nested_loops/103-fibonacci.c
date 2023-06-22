501ac9d23f7aad743d21fae5894a2ef697031a85
Billrawknow <bildadronoh@gmail.com>
2023-06-22 08:31:45 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,24 +7,24 @@
  */
 int main(void)
 {
-    int first = 1;
-    int second = 2;
-    int next;
-    int sum = 2; /* Start with the sum as 2 (since the second term is even) */
+	int first = 1;
+	int second = 2;
+	int next;
+	int sum = 2; /* Start with the sum as 2 (since the second term is even) */
 
-    while (second <= 4000000)
-    {
-        next = first + second;
-        if (next % 2 == 0)
-        {
-            sum += next;
-        }
+	while (second <= 4000000)
+	{
+		next = first + second;
+		if (next % 2 == 0)
+		{
+			sum += next;
+		}
 
-        first = second;
-        second = next;
-    }
+		first = second;
+		second = next;
+	}
 
-    printf("Sum of even-valued terms: %d\n", sum);
+	printf("Sum of even-valued terms: %d\n", sum);
 
-    return 0;
+	return (0);
 }
