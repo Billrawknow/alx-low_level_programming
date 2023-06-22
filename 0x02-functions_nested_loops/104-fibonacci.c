4a935ecb8c2e568338854ea07db2900dcfb5eb13
Billrawknow <bildadronoh@gmail.com>
2023-06-22 09:39:55 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,32 +1,27 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Description: Prints the first 98 Fibonacci numbers
- *              in a specific format
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-    int count = 2;   /* Number of Fibonacci numbers to generate (excluding 1 and 2) */
-    int prev = 1;    /* Previous Fibonacci number */
-    int curr = 2;    /* Current Fibonacci number */
+    int count = 2; /* Number of Fibonacci numbers to generate (excluding 1 and 2) */
+    int prev = 1; /* Previous Fibonacci number */
+    int curr = 2; /* Current Fibonacci number */
 
-    printf("%d, %d", prev, curr); /* Print the first two Fibonacci numbers */
+    printf("%d, %d, ", prev, curr); /* Print the first two Fibonacci numbers */
 
     while (count < 98)
     {
         int next = prev + curr; /* Calculate the next Fibonacci number */
-        printf(", %d", next);
+        printf("%d", next);
+
+        if (count != 97) /* Check if it's the last number */
+            printf(", ");
+        else
+            printf("\n");
 
         count++;
         prev = curr;
         curr = next;
     }
-
-    printf("\n");
 
     return 0;
 }
