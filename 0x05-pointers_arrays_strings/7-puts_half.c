cc838ba365f24b446c3358dffae60838d1739e1b
Billrawknow <bildadronoh@gmail.com>
2023-06-26 12:39:38 -0700

    7-puts

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+{
+	int len, x, i;
+
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else if (len % 2)
+	{
+		for (x = (len - 1) / 2; x < len - 1; x++)
+		{
+			_putchar(str[x + 1]);
+		}
+	}
+	_putchar('\n');
+}
