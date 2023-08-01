311415630dd2768d885ee205ed09def919289c0a
Billrawknow <bildadronoh@gmail.com>
2023-08-01 11:19:36 -0700

    4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,47 +1,19 @@
+#include <stdlib.h>
 #include "lists.h"
+
 /**
- * add_nodeint_end - add a node at the end of the list
- * @head: pointer to the first node in the list
- * @n: data to be added in the new node
- * Return: pointer to new node or NULL incase it fails
- */
-listint_t *add_nodeint_end(listint_t **head, const int n)
-{
-	listint_t *newnode;
-	listint_t *temp = *head;
-
-	newnode = malloc(sizeof(listint_t));
-
-			if (!newnode)
-			return (NULL);
-
-			newnode->n = n;
-			newnode->next = NULL;
-
-			if (*head == NULL)
-			{
-			*head = newnode;
-			return (*head);
-			}
-
-			while (temp->next)
-			temp = temp->next;
-
-			temp->next = newnode;
-			return (*head);
-			#include "lists.h"
-/**
- * free_listint - free linked list
- * @head: pointer to first node of a linked list that is being freed
+ * free_listint - Frees the memory allocated for a linked listint_t list.
+ * @head: Pointer to the head of the linked list.
  */
 void free_listint(listint_t *head)
 {
-	listint_t *temp;
+    listint_t *current;
 
-	while (head)
-	{
-		temp = head->next;
-		free(head);
-		head = temp;
-	}
-}}
+    while (head != NULL)
+    {
+        current = head;
+        head = head->next;
+        free(current);
+    }
+}
+
